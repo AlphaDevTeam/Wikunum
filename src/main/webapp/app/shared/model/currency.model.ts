24cf08eb9ff9a5b12252107d44b03569934c9ef7
AlphaDevTeam <info@alphadevs.com>
2019-11-24 15:57:43 +0530

    Revert "Add entities using the JDL model `Wikunum`"

--- src/main/webapp/app/shared/model/currency.model.ts
+++
@@ -1,9 +0,0 @@
-export interface ICurrency {
-  id?: number;
-  currencyCode?: string;
-  currencyName?: string;
-}
-
-export class Currency implements ICurrency {
-  constructor(public id?: number, public currencyCode?: string, public currencyName?: string) {}
-}
